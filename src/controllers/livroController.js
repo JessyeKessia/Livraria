2f74ec9c202e38d97811f1f214b2893c861f8696
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-27 20:48:42 -0300

    :sparkles: feat: adicionado métodos cadastrar, buscar, atualizar e listar livros por editora ao código

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,5 +1,6 @@
 // Serve para fazer o controle dos métodos
 // Os controllers atuam como intermediários entre a camada responsável pelo recebimento dos inputs de dados, que em nossa API está sendo feito através das rotas, e os models. Controllers também são a camada responsável pelo “caminho inverso”, pegando o resultado do processamento feito pelos models e “repassando” de volta.
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js"
 
 class LivroController {
@@ -8,14 +9,84 @@ class LivroController {
         try {
             // controller chama o model Livro com o find({})
             const listaLivros = await livro.find({});
-            res.status(200).json(listaLivros);
+            res
+                .status(200)
+                .json(listaLivros);
         } catch(erro) {
             res
                 .status(500)
-                .json({mensage: $`{erro.message} - falha na requisição`});
+                .json({mensage: `${erro.message} - falha na requisição`});
+        }
+    };
+    
+    static async cadastrarLivro (req, res) {
+        const novoLivro = req.body;
+        try {
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+            const livroCriado = await livro.create(livroCompleto);
+            res
+                .status(201)
+                .json({message: `Livro criado com sucesso`})
+        } catch(erro) {
+            res
+                .status(500)
+                .json({mensage: `${erro.message} - falha ao cadastrar livro`})
+        }
+    }
+    static async buscarLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await livro.findById(id);
+            res
+                .status(200)
+                .json(livroEncontrado);
+        } catch(erro) {
+            res
+                .status(500)
+                .json({mensage: `${erro.message} - falha na requisição do livro`});
+        }
+    };
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res
+                .status(200)
+                .json({menssagem: "Livro atualizado"});
+        } catch(erro) {
+            res
+                .status(500)
+                .json({mensage: `${erro.message} - falha na atualização do livro`});
+        }
+    };
+    static async deletarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res
+                .status(200)
+                .json({menssagem: "Livro removido com sucesso"});
+        } catch(erro) {
+            res
+                .status(500)
+                .json({mensage: `${erro.message} - falha na deleção do livro`});
+        }
+    };
+    static async listarLivrosPorEditora(req, res) {
+        const editora = req.query.editora
+        try {
+            const livrosPorEditora = await livro.find({ editora })
+            res
+                .status(200)
+                .json(livrosPorEditora)
+        } catch (erro) {
+            res
+                .status(500)
+                .json({mensage: `${erro.message} - falha na busca`});
         }
     }
 
-};
+}
 
 export default LivroController;
