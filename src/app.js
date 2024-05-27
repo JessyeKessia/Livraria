8b7b88f05ba9158ad9dec2cdfc6f3b2188044bb0
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-27 20:45:18 -0300

    :zap: perf: Melhoria na organização do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import connectNaDataBase from '../src/config/dbConnect.js';
-import livro from "./models/Livro.js"
+import routes from "./routes/index.js"
 
 // banco de dados
 const conexao = await connectNaDataBase();
@@ -14,34 +14,6 @@ conexao.once("open", () => {
 })
 
 const app = express();
-app.use(express.json());
-
-// rota home
-app.get("/", (req, res) => {
-    res.status(200).send("livraria");
-})
-
-// Método de busca
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    res.status(200).json(livros[index])
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).send("Livro cadastrado com sucesso")
-})
-
-app.put("/livros/:id", (req, res) =>{
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo; // titulo antigo = titulo novo(body.nome é a requisicao)
-    res.status(200).json(livros)
-})
-
-app.delete("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros.splice(index, 1);
-    res.status(200).send("Livro removido com sucesso");
-});
+routes(app);
 
 export default app;
