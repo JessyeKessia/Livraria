8893145611e07e0512d8f7303c8521e18f97e2b2
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-27 20:51:44 -0300

    :sparkles: feat: Adicionando rota dos métodos

--- src/routes/livroRoutes.js
+++ src/routes/livroRoutes.js
@@ -1,7 +1,16 @@
 // serve para estabelecer as rotas
-import LivroController from "../controllers/livroController";
+import LivroController from "../controllers/livroController.js";
 import express from "express";
 
 const routes = express.Router();
 
-routes.get("/livros", LivroController.listarLivros)
+routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
+routes.get("/livros/:id", LivroController.buscarLivroPorId);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.deletarLivro);
+
+
+
+export default routes;
