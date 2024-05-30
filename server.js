546751b87b6a17c56c20dd7d9d992effd30a07c1
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-29 22:21:45 -0300

    :wrench:: baixando elementos e adicionando elementos faltantes

--- server.js
+++ server.js
@@ -2,20 +2,6 @@ import "dotenv/config";
 import app from "./src/app.js";
 const PORT = 3000;
 
-// const rotas = {
-//     // fazendo rotas
-//     "/": "Livraria",
-//     "/livros": "Entrei na rota livros",
-//     "/autores": "Entrei na rota autores"
-// }
-
-// criando o server
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, {"Content-Type": "text/plain"});
-//     res.end(rotas[req.url]);
-//     // o url é uma requisição do req, que pega as rotas da url
-// })
-
 app.listen(PORT, () => {
     console.log("Servidor escutando!")
 })
