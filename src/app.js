51f987799ab65af74eb69ab1e6d6475ddd1c2d2c
Jesye Pereira <jessyekessia16@gmail.com>
2024-05-30 00:25:50 -0300

    :goal_net: Adiciondo o tratamento de erro do 404

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import connectNaDataBase from '../src/config/dbConnect.js';
 import routes from "./routes/index.js"
 import manipuladorDeErros from './middlewares/manipuladorDeErros.js';
+import manipulador404 from './middlewares/manipulador404.js';
 
 // banco de dados
 const conexao = await connectNaDataBase();
@@ -18,6 +19,9 @@ const app = express();
 app.use(express.json());
 routes(app);
 
-app.use(manipuladorDeErros)
+app.use(manipulador404)
+
+app.use(manipuladorDeErros);
+
 
 export default app;
